555e35f4f1f1057e105e15e9fb63c079cba5d283
Aquiba Yudah Benarroch Bittan <a01783710@tec.mx>
2024-09-17 15:49:36 -0600

    Prueba 1

---
+++ Prueba.cpp
@@ -0,0 +1,9 @@
+//Este es un documento de prueba
+#include <iostream>
+int main()
+{
+    int a = 8;
+    int b = 10;
+    std::cout << "La multiplicacion de " << a << " con " << b << " es: " << a*b << "\n";
+    return 0;
+}
