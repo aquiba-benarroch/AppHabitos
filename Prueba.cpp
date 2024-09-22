066c3a4192790c44be0e82daa48eb0b03ae9ffcf
Aquiba Yudah Benarroch Bittan <142257549+aquiba-benarroch@users.noreply.github.com>
2024-09-22 10:43:35 -0600

    Delete Prueba.cpp

--- Prueba.cpp
+++
@@ -1,9 +0,0 @@
-//Este es un documento de prueba
-#include <iostream>
-int main()
-{
-    int a = 8;
-    int b = 10;
-    std::cout << "La multiplicacion de " << a << " con " << b << " es: " << a*b << "\n";
-    return 0;
-}
